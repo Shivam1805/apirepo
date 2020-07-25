e60eabc59da38d8ba010f117928b3710b5ba9623
Shivam Gupta <shivam.gupta@dal.ca>
2020-07-25 18:42:05 -0300

    add

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 
 const mySql = require('mysql');
 
-app.use(cors({ origin: true }));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
